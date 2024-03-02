25dbac1ac5df538be55f10637fe6b5744bb26c64
Amit Singh <c.a.singh@hotmail.com>
2024-03-02 17:17:54 -0500

    First commit, init testing framework

---
+++ Tests/ConversionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ConversionTest extends TestCase {
+    public function testConversion()
+    {
+        $this->assertTrue(true);
+    }
+}
