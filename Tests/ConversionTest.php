619a0defe2630717cdd43b0be832cd1daf6c3673
Amit Singh <c.a.singh@hotmail.com>
2024-03-02 18:10:48 -0500

    Add sample test

--- Tests/ConversionTest.php
+++ Tests/ConversionTest.php
@@ -2,11 +2,27 @@
 
 namespace Tests;
 
+use Amit\Mc\Parser;
 use PHPUnit\Framework\TestCase;
 
+
+/**
+ * @uses Parser
+ * (optional)@covers Parser
+ */
 class ConversionTest extends TestCase {
     public function testConversion()
     {
+        $p = new Parser();
+        $files = array_diff(scandir(__DIR__ . '/data/'), ['.', '..']);
+        foreach($files as $f) {
+            if (preg_match('/.?\.md/', $f)) {
+                $input = fopen(__DIR__ . '/' . $f, 'r');
+                $parts = explode('.', $f);
+                $output = fopen(__DIR__ . '/' . $parts[0] . '.html', 'r');
+                $this->assertEquals($output, $p->parse($input));
+            }
+        }
         $this->assertTrue(true);
     }
 }
